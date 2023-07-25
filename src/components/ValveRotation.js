fff2940ce68a647a4cb55b685adcebe0e4a0a0da
Alexander-Gronninger <alexander.gronninger@gmail.com>
2023-07-25 17:30:35 +0200

    setup rotation component

--- src/components/ValveRotation.js
+++ src/components/ValveRotation.js
@@ -1,37 +1,115 @@
-import React, { useState } from "react";
-import Draggable from "react-draggable";
+import React, { useState, useRef } from "react";
 
 const ValveRotation = () => {
+  const imageRef = useRef(null);
   const [rotation, setRotation] = useState(0);
-  const [size, setSize] = useState(1);
+  const [opening, setOpening] = useState(1);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [prevY, setPrevY] = useState(0);
+  const [fullRotations, setFullRotations] = useState(0);
 
-  const handleDrag = (_, { deltaX }) => {
-    // Calculate the new rotation angle based on the drag
-    setRotation((prevRotation) => prevRotation + deltaX);
+  /* Minimum and maximum opening allowed (1-10 mm) */
+  const minOpening = 1;
+  const maxOpening = 10;
+  /* The amount of degrees that 1 in opening corresponds to */
+  const degreesPerOpening = 360;
+
+  const handleMouseDown = (event) => {
+    event.preventDefault();
+    setIsMouseDown(true);
   };
 
-  const handleSizeChange = (e) => {
-    const newSize = parseFloat(e.target.value);
-    setSize(newSize);
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isMouseDown) return;
+
+    // Calculate the new rotation value based on difference between center and mouse position
+    let angle = calculateRotationAngle(event);
+
+    // Calculate the number of full rotations completed
+    /* If user is going towards angle of 0, rotations is at 0, and the mouse is moving down, hit minimum limit */
+    if (angle < 5 && fullRotations === 0 && event.pageY > prevY) {
+      console.log("MINIMUM TEST");
+      setIsMouseDown(false);
+      return;
+    }
+    /* If user is going towards angle of 360, rotations is at max and mouse is moving up, hit max limit */
+    if (
+      angle === 360 &&
+      fullRotations === maxOpening - minOpening &&
+      event.pageY < prevY
+    ) {
+      console.log("MAXUMUM TEST");
+      setIsMouseDown(false);
+      return;
+    }
+
+    /* If user is going towards angle of 0, rotations is at >0, and the mouse is moving down, decrease by 1 */
+    if (angle < 5 && fullRotations >= 0 && event.pageY > prevY) {
+      console.log("Decreasing fullRotations by 1");
+      setFullRotations(fullRotations - 1);
+    }
+    /* If user is going towards angle of 360, rotations is not at max and mouse is moving up, increase by 1 */
+    if (
+      angle === 360 &&
+      fullRotations <= maxOpening - minOpening &&
+      event.pageY < prevY
+    ) {
+      console.log("Increasing fullRotations by 1");
+      setFullRotations(fullRotations + 1);
+    }
+
+    setRotation(angle);
+
+    // Calculate new Opening value, minus fullRotations,
+    setOpening(
+      (fullRotations + minOpening + angle / degreesPerOpening).toFixed(2)
+    );
+
+    // Update the previous Y-coordinate with the current Y-coordinate, so we can determine if mouse is moving up or down
+    setPrevY(event.pageY);
+  };
+
+  const calculateRotationAngle = (event) => {
+    /* Calculates the x and y position of the center of the rotating div */
+    const centerX =
+      imageRef.current.offsetLeft + imageRef.current.offsetWidth / 2;
+    const centerY =
+      imageRef.current.offsetTop + imageRef.current.offsetHeight / 2;
+
+    /* Difference between center and mouse position */
+    const deltaX = event.pageX - centerX;
+    const deltaY = event.pageY - centerY;
+
+    // Calculate the new rotation value based on difference between center and mouse position
+    return (Math.atan2(deltaY, deltaX) * 180) / Math.PI + 180;
   };
 
   return (
-    <div
-      className="w-fit h-fit rounded-[50%] origin-center cursor-grab m-0"
-      style={{ transform: `rotate(${rotation}deg)` }}
-    >
-      <Draggable
-        axis="x"
-        onDrag={handleDrag}
-        className="m-0 block w-[25px] h-[25px]"
+    <>
+      <div
+        className="rounded-full border-black shadow-[0px_0px_10px_2px] shadow-black"
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          width: `50px`,
+          height: `50px`,
+        }}
+        ref={imageRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       >
         <img
           src="../images/Pencil-Circle.png"
-          alt="Rotatable Image"
-          className={`w-${size * 10} h-${size * 10}  m-0`}
+          alt=""
+          className={`w-{50px} h-{50px}  m-0`}
         />
-      </Draggable>
-    </div>
+      </div>
+      <div className="text-center">{`Opening: ${opening}`}</div>
+    </>
   );
 };
 
