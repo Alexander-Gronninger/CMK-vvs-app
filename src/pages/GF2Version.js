eabd9f3193ca4c5f36ac90d92ef379f7b567604b
Alexander-Gronninger <alexander.gronninger@gmail.com>
2023-12-05 16:32:22 +0100

    Update GF2Version.js

--- src/pages/GF2Version.js
+++ src/pages/GF2Version.js
@@ -18,11 +18,11 @@ const GF2Version = () => {
   const [test, setTest] = useState(0);
 
   useEffect(() => {
-    const updateTest = () => {
+    /* const updateTest = () => {
       setTest(chartContainerRef.current?.offsetHeight);
     };
 
-    updateTest();
+    updateTest(); */
   }, []);
 
   console.log(test);
@@ -76,7 +76,10 @@ const GF2Version = () => {
             />
             <div
               ref={chartContainerRef}
-              onLoad={() => setTest(chartContainerRef.current?.offsetHeight)}
+              onLoad={() => {
+                setTest(chartContainerRef.current?.offsetHeight);
+                console.log(chartContainerRef);
+              }}
               className="row-start-1 row-end-1 col-start-1 col-end-1 grid h-[75%] items-end w-full gap-4 grid-cols-5 grid-rows-[13fr,3fr]"
             >
               {/* The component handling the dots, their positioning and animation */}
