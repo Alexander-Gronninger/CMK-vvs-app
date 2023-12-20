f36fa027cc19e6a2b288b9e15d4be11d361140e2
Alexander-Gronninger <alexander.gronninger@gmail.com>
2023-12-20 15:00:22 +0100

    cleanup 2, move toastify to app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import ExportSettings from "./pages/ExportSettings";
 import Ref from "./pages/Ref";
 import GF2Version from "./pages/GF2Version";
 import GF2Teacher from "./pages/GF2Teacher";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <ContextProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Routes>
         <Route path="/" element={<GF2Version />} key="GF2Version" />
         <Route path="/export" element={<ExportSettings />} key="Export" />
