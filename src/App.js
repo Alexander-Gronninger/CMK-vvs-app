e3836eb7d37b982de1079c47f08f640dc009a934
Alexander-Gronninger <alexander.gronninger@gmail.com>
2023-12-28 12:39:38 +0100

    update to reflect main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import ContextProvider from "./context/ContextProvider";
 import Ref from "./pages/Ref";
 import GF2Version from "./pages/GF2Version";
-import GF2Teacher from "./pages/GF2Teacher";
+/* import GF2Teacher from "./pages/GF2Teacher"; */
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./templates/Footer";
+import Header from "./templates/Header";
 
 function App() {
   return (
@@ -23,10 +27,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<GF2Version />} key="GF2Version" />
-        <Route path="/export" element={<ExportSettings />} key="Export" />
-        <Route path="/ref" element={<Ref />} key="Ref" />
-        <Route path="teacher" element={<GF2Teacher />} key="GF2Teacher" />
-        <Route path="/ref" element={<Ref />} key="Ref" />
+        {/* <Route path="teacher" element={<GF2Teacher />} key="GF2Teacher" /> */}
+        <Route path="ref" element={<Ref />} key="Ref" />
         <Route path="*" element={<Navigate to="/" />} key="NotFound" />
       </Routes>
       <Footer />
